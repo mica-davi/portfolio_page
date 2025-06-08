a6f64fbaa25f81d90250fa6ab4f692e24b90763d
Davi Mica <davimica04@gmail.com>
2025-06-08 13:21:38 -0300

    public images fixed

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,8 +3,8 @@ import "../css/footer.css"
 function Footer(){
     return (
         <div className="footer">
-            <img className="icon" src="../../public/icons8-github-128.png" alt="github" onClick={() => {window.open("https://github.com/mica-davi", "_blank")}}></img>
-            <img className="icon" src="../../public/icons8-linkedin-128.png" alt="linkedin" onClick={() => {window.open("https://www.linkedin.com/in/dav%C3%AD-michaelsen-9ab893231/", "_blank")}}></img>
+            <img className="icon" src="icons8-github-128.png" alt="github" onClick={() => {window.open("https://github.com/mica-davi", "_blank")}}></img>
+            <img className="icon" src="icons8-linkedin-128.png" alt="linkedin" onClick={() => {window.open("https://www.linkedin.com/in/dav%C3%AD-michaelsen-9ab893231/", "_blank")}}></img>
         </div>
     )
 }
